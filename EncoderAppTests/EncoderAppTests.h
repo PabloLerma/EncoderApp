4741e2118d29c906d8e8c69d1162a3895cf0e5c7
Pablo Lerma Martínez <Paul@MacBook-Pro-de-Pablo.local>
2011-11-26 04:39:45 +0100

    Initial Commit

---
+++ EncoderAppTests/EncoderAppTests.h
@@ -0,0 +1,13 @@
+//
+//  EncoderAppTests.h
+//  EncoderAppTests
+//
+//  Created by Yo on 26/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EncoderAppTests : SenTestCase
+
+@end
---
+++ EncoderAppTests/EncoderAppTests.m
@@ -0,0 +1,32 @@
+//
+//  EncoderAppTests.m
+//  EncoderAppTests
+//
+//  Created by Yo on 26/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "EncoderAppTests.h"
+
+@implementation EncoderAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EncoderAppTests");
+}
+
+@end

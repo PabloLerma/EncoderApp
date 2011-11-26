d379785e21bf15c55a2728827fc8e3759fdee505
Pablo Lerma Martínez <Paul@MacBook-Pro-de-Pablo.local>
2011-11-26 20:56:14 +0100

    First Version

--- EncoderApp/AppDelegate.h
+++ EncoderApp/AppDelegate.h
@@ -8,8 +8,20 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+    IBOutlet NSTextField *url;
+    IBOutlet NSTextField *shorturl;
+    IBOutlet NSImageView *imageview;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (retain, atomic) IBOutlet NSTextField *url;
+@property (retain, atomic) IBOutlet NSTextField *shorturl;
+@property (retain, atomic) IBOutlet NSImageView *imageview;
+
+
+-(IBAction)encode:(id)sender;
+-(IBAction)shorturl:(id)sender;
+-(IBAction)saveimage:(id)sender;
 
 @end
--- EncoderApp/AppDelegate.m
+++ EncoderApp/AppDelegate.m
@@ -11,10 +11,27 @@
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize url,shorturl,imageview;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    [_window setBackgroundColor:[NSColor lightGrayColor]];
+}
+
+-(IBAction)shorturl:(id)sender
+{
+    
+}
+
+-(IBAction)encode:(id)sender
+{
+
+}
+
+-(IBAction)saveimage:(id)sender
+{
+    
 }
 
 @end

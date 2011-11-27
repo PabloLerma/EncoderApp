87e813a4a3728c916b3ed15debb66739a108d900
Pablo Lerma Martínez <Paul@MacBook-Pro-de-Pablo.local>
2011-11-27 03:10:14 +0100

    Segunda version

--- EncoderApp/AppDelegate.h
+++ EncoderApp/AppDelegate.h
@@ -12,16 +12,22 @@
     IBOutlet NSTextField *url;
     IBOutlet NSTextField *shorturl;
     IBOutlet NSImageView *imageview;
+    NSMutableData *receivedData;
 }
 
 @property (assign) IBOutlet NSWindow *window;
-@property (retain, atomic) IBOutlet NSTextField *url;
-@property (retain, atomic) IBOutlet NSTextField *shorturl;
-@property (retain, atomic) IBOutlet NSImageView *imageview;
+@property (retain, nonatomic) IBOutlet NSTextField *url;
+@property (retain, nonatomic) IBOutlet NSTextField *shorturl;
+@property (retain, nonatomic) IBOutlet NSImageView *imageview;
+@property (retain, nonatomic)  NSMutableData *receivedData;
 
 
 -(IBAction)encode:(id)sender;
 -(IBAction)shorturl:(id)sender;
 -(IBAction)saveimage:(id)sender;
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response;
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection;
 
 @end
--- EncoderApp/AppDelegate.m
+++ EncoderApp/AppDelegate.m
@@ -11,7 +11,7 @@
 @implementation AppDelegate
 
 @synthesize window = _window;
-@synthesize url,shorturl,imageview;
+@synthesize url,shorturl,imageview,receivedData;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -21,12 +21,80 @@
 
 -(IBAction)shorturl:(id)sender
 {
+    NSString *stringurl = [url stringValue];
+    if ([stringurl length] == 0){
+        [shorturl setStringValue:@"It is not valid a empty URL"];
+    }
+    else {
+        if ([stringurl hasPrefix:@"http://"]){
+            NSString *prefix = @"http://is.gd/create.php?format=simple&url=";
+            NSString *web = [NSString stringWithFormat:@"%@%@", prefix, stringurl];
+            NSData *htmlFile = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:web]];
+            NSString *htmlString = [[NSString alloc] initWithData:htmlFile encoding:NSUTF8StringEncoding];
+            [shorturl setStringValue:htmlString];
+        }
+        else {
+            [shorturl setStringValue:@"URL need start with \"http://\""];
+        }
+        
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    // This method is called when the server has determined that it
+    // has enough information to create the NSURLResponse.
     
+    // It can be called multiple times, for example in the case of a
+    // redirect, so each time we reset the data.
+    
+    // receivedData is an instance variable declared elsewhere.
+    [receivedData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    // Append the new data to receivedData.
+    // receivedData is an instance variable declared elsewhere.
+    [receivedData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    // release the connection, and the data object
+    //[connection release];
+    // receivedData is declared as a method instance elsewhere
+   // [receivedData release];
+    
+    // inform the user
+    NSLog(@"Connection failed! Error - %@ %@",
+          [error localizedDescription],
+          [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // do something with the data
+    // receivedData is declared as a method instance elsewhere
+    NSLog(@"Succeeded! Received %@ bytes of data",[receivedData length]);
+    
+    // release the connection, and the data object
+    //[connection release];
+    //[receivedData release];
 }
 
 -(IBAction)encode:(id)sender
 {
-
+    [imageview setImage:nil];
+    //[imageview lockFocus];
+    NSString *prueba = @"This is a text";
+    NSImage *newimg;
+    [newimg lockFocus];
+    NSPoint point = NSMakePoint(10, 100);
+    [prueba drawAtPoint:point withAttributes:nil];
+    //[imageview unlockFocus];
+    [newimg unlockFocus];
+    [imageview setImage:newimg];
 }
 
 -(IBAction)saveimage:(id)sender
